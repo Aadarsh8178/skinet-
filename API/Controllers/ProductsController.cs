291e74d656c57314d895519ebacc7dea3a45af6b
Aadarsh Verma <vaadarsh8178@gmail.com>
2021-06-26 21:54:30 +0530

    pagination/search/cors

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
+using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -27,12 +28,15 @@ namespace API.Controllers
       _productTypeRepo = productTypeRepo;
     }
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+    public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery] ProductSpecParams productParams)
     {
-      var spec = new ProductswithTypesAndBrandsSpecification();
+      var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+      var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+      var totalItems = await _productsRepo.CountAsync(countSpec);
       var products = await _productsRepo.ListAsync(spec);
+      var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
-      return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+      return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
     }
 
     [HttpGet("{id}")]
@@ -40,7 +44,7 @@ namespace API.Controllers
     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
-      var spec = new ProductswithTypesAndBrandsSpecification(id);
+      var spec = new ProductsWithTypesAndBrandsSpecification(id);
       var product = await _productsRepo.GetEntityWithSpec(spec);
       if (product == null) return NotFound(new ApiResponse(404));
       return _mapper.Map<Product, ProductToReturnDto>(product);
